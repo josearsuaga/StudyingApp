56b94d75c3e294932194b2b9ce6ce7ce1e63272b
Jose <>
2024-07-05 18:09:53 -0600

    initial commit

---
+++ StudyingApp/StudyingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyingAppApp.swift
+//  StudyingApp
+//
+//  Created by José Arsuaga Sotres on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
